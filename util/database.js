304e2571708b8761fbbab284a4b23f4e28e36683
unknown <tom_ski18@yahoo.com>
2022-04-02 00:22:42 +0200

    nosql p1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,32 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'tomskibt', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://tome:tome@cluster0.rjx0e.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client => {   
+    console.log('Connected');
+    _db = client.db();
+    callback(); 
+  })
+  .catch(err => {
+    console.log(err)
+    throw err;
+  });
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No db found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+
+
+
+
